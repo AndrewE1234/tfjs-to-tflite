6cadd80452be1c1ad5c67f883a77227a2a6db385
AndrewE1234 <andrew@omnicode.com>
2021-01-05 15:06:40 -0800

    added script

---
+++ tfjs-to-tflite.py
@@ -0,0 +1,31 @@
+# tflite-converter
+# Andrew Eljumaily 2021
+#
+# A tfjs-layers to tflite converter
+
+import sys
+import os
+
+if len(sys.argv) <= 1:
+  sys.exit("ERROR: Must include path to input file as argument.\nEX: python {} models/model.json".format(sys.argv[0]))
+
+fileName = sys.argv[1]
+if fileName == "-h" or fileName == "--help":
+  print("tflite-converter, Andrew Eljumaily 2021\nA simple to use tfjs-layers to tflite converter\n\n Usage: {} <path/to/input/file.json>\n Output file will share the same name as input.".format(sys.argv[0]))
+  sys.exit()
+
+# Convert a tfjs layers file to a keras file using tensorflowjs_converter (part of tesnorflowjs)
+os.system("tensorflowjs_converter --input_format=tfjs_layers_model --output_format=keras {} test1/saved_model.h5".format(fileName))
+
+import tensorflow as tf
+
+# Load a model using high-level tf.keras.* APIs
+model = tf.keras.models.load_model('test1/saved_model.h5')
+
+# Convert the model.
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model1.tflite', 'wb') as f:
+  f.write(tflite_model)
